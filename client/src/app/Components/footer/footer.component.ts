6b48850fe4a0ac859345931f5e5dcfd77eba872d
ocanades <ocanades@gmail.com>
2020-08-11 19:57:25 +0200

    toDo: build header and footer

--- client/src/app/Components/footer/footer.component.ts
+++ client/src/app/Components/footer/footer.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  // TRANSLATE
+  selectedLanguage = 'es';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private translateService: TranslateService) {
+    //translate
+    this.translateService.setDefaultLang(this.selectedLanguage);
+    this.translateService.use(this.selectedLanguage);
   }
 
+  //translate
+  toogleLanguage(lang: string) {
+    this.translateService.use(lang);
+  }
+
+  ngOnInit(): void {}
 }
